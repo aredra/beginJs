6d828d06e34db97915950eeaf75201ca64c7c06d
aredra <rnswn48@gmail.com>
2020-06-22 10:04:35 +0900

    Added merge sort

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,4 +1,4 @@
-function bubble(arr) {
+function bubbleSort(arr) {
     let result = arr.slice();
 
     for (let i=0; i < result.length-1; i++) {
@@ -15,6 +15,37 @@ function bubble(arr) {
      return result
 }
 
+function mergeSort(arr) {    
+    if (arr.length <= 1) { return arr;}
+    const mid = Math.floor(arr.length/2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+    // console.log(left);
+    // console.log(right);
+    
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    console.log(left);
+    console.log(right);
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    while (left.length) {
+        result.push(left.shift());
+    }
+    while (right.length) {
+        result.push(right.shift());
+    }
+    return result;
+}
+
 const arr = [1, 10, 2, 5, 2, 1, 8, 4];
 
-console.log(bubble(arr));
+console.log(mergeSort(arr));
