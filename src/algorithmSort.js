5c2dd4c2bbaac3cc6544321120507430e5910ae0
aredra <rnswn48@gmail.com>
2020-07-01 23:07:10 +0900

    Add quick sort

--- src/algorithmSort.js
+++ src/algorithmSort.js
@@ -3,8 +3,6 @@ function bubbleSort(arr) {
 
     for (let i=0; i < result.length-1; i++) {
         for (let j=0; j < result.length -1 -i; j++) {
-            console.log(j);
-            console.log(result[j+1]);
             if (result[j] > result[j+1]) {
                 let val = result[j];
                 result[j] = result[j+1];
@@ -20,16 +18,13 @@ function mergeSort(arr) {
     const mid = Math.floor(arr.length/2);
     const left = arr.slice(0, mid);
     const right = arr.slice(mid);
-    // console.log(left);
-    // console.log(right);
     
     return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
     let result = [];
-    console.log(left);
-    console.log(right);
+
     while (left.length && right.length) {
         if (left[0] < right[0]) {
             result.push(left.shift());
@@ -46,6 +41,22 @@ function merge(left, right) {
     return result;
 }
 
+function quickSort(arr) {
+    if (arr.length <=1) return arr;
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i=1; i<arr.length; i++) {
+        if (arr[i] > pivot) {
+            right.push(arr[i]);
+        } else {
+            left.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
 const arr = [1, 10, 2, 5, 2, 1, 8, 4];
 
-console.log(mergeSort(arr));
+console.log(quickSort(arr));
