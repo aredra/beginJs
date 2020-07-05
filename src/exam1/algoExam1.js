901faf3c249f3269aedd569ae4ce911536fb9078
aredra <rnswn48@gmail.com>
2020-07-05 23:40:39 +0900

    Add hanoi function

--- src/exam1/algoExam1.js
+++ src/exam1/algoExam1.js
@@ -23,4 +23,22 @@ function mathBrackets(arr) {
 
 const arr = "(()(()))()";
 
-console.log(mathBrackets(arr));
+// console.log(mathBrackets(arr));
+
+const route = [];
+
+function hanoi(num, start, end, temp) {
+  if (num === 1) {
+    route.push([start, end]);
+    return NaN;
+  }
+
+  hanoi(num-1, start, temp, end);
+  route.push([start, end]);
+  hanoi(num-1, temp, end, start);
+}
+
+console.log(hanoi(4, "A", "C", "B"));
+console.log(route);
+
+
