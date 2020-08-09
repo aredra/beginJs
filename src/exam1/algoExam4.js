e3b9c01a35527ec7cba93ee988c85bcdeec4ebfe
aredra <rnswn48@gmail.com>
2020-08-10 00:12:40 +0900

    Add LRU and spin matrix

--- src/exam1/algoExam4.js
+++ src/exam1/algoExam4.js
@@ -1,4 +1,4 @@
-//페이징 FIFO
+//페이징 FIFO, LRU, 행렬 돌리기
 
 function pagingCount(page, size, noneTime, existTime) {
     let runtime = 0;
@@ -12,6 +12,10 @@ function pagingCount(page, size, noneTime, existTime) {
     for (let i of page) {
         if (temp.includes(i)) {
             runtime += existTime;
+            // LRU
+            // const idx = temp.indexOf(i);
+            // const pp = temp.splice(idx, 1);
+            // temp.push(pp[0]);
         } else {
             runtime += noneTime;
             temp.push(i);
@@ -20,7 +24,36 @@ function pagingCount(page, size, noneTime, existTime) {
             }
         }
     }
-
     return runtime;
 }
-console.log(pagingCount('ABIEIBEABEJ', 4, 6, 1));
+console.log(pagingCount('IEIFIEFHAFIEIFEH', 5, 6, 1));
+
+function rotateMatrix(matrix) {
+    const s = matrix.length;
+    let rotate = [];
+
+    for (let i=0; i<s; i++) {
+        rotate.push(Array(s).fill(0));
+    }
+
+    for (let i=0; i<s; i++) {
+        for (let j=0; j<s; j++) {
+            rotate[j][s-1-i] = matrix[i][j];
+        }
+    }
+
+    for (let i = 0;i < s;i++) {
+        for (let j = 0;j < s;j++) {
+            rotate[i][j] += matrix[i][j];
+        }
+    }
+
+    return rotate;
+}
+const mat = [
+    [1,1,1,2],
+    [2,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+]
+console.log(rotateMatrix(mat));
