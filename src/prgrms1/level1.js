318d51e41589751a4d0dd1a3e9bd9684327906d1
aredra <rnswn48@gmail.com>
2020-09-02 21:55:31 +0900

    Add etc

--- src/prgrms1/level1.js
+++ src/prgrms1/level1.js
@@ -11,3 +11,32 @@ function hateSameNum(arr) {
     }, []);
 }
 
+//번외 같은 숫자 만큼 무료
+let totalCnt = 0;
+const freeUnit = 5;
+function setTotalCount(value, fare) {
+    totalCnt += value;
+    const tmpCnt = Math.floor(totalCnt / freeUnit);
+    const tmpRemain = totalCnt % freeUnit;
+    let realCnt = 0;
+    let freeCnt = 0;
+    let cntStr = "";
+
+    if (tmpCnt % 2 == 0) {
+        realCnt += tmpRemain;
+        realCnt += Math.floor(tmpCnt / 2) * freeUnit;
+        freeCnt += Math.floor(tmpCnt / 2) * freeUnit;
+    } else {
+        freeCnt += tmpRemain;
+        realCnt += (Math.floor(tmpCnt / 2) + 1) * freeUnit;
+        freeCnt += Math.floor(tmpCnt / 2) * freeUnit;
+    }
+
+    cntStr += freeCnt == 0 ? realCnt : realCnt + " (+" + freeCnt + ")";
+    totalFare = fare * realCnt;
+
+    return {
+        resultText: cntStr,
+        fare: totalFare
+    }
+}
