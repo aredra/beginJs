7a11d0fda96738fe5736997c48e3d3e548d868d1
aredra <rnswn48@gmail.com>
2020-09-10 23:59:34 +0900

    Add programmars level 1

--- src/prgrms1/level1.js
+++ src/prgrms1/level1.js
@@ -103,3 +103,106 @@ function customStringSort(strings, n) {
 function stringDesc(s) {
   return s.split("").sort().reverse().join("");
 }
+
+//시저 암호
+function caesarCipher(s, n) {
+  return s
+    .split("")
+    .map((el) => {
+      let tempAscii = 0;
+      if (el === " ") {
+        return el;
+      } else if (el.charCodeAt(0) > 64 && el.charCodeAt(0) < 91) {
+        tempAscii =
+          el.charCodeAt(0) + n > 90
+            ? Math.abs(el.charCodeAt(0) + n - 90) + 64
+            : el.charCodeAt(0) + n;
+        return String.fromCharCode(tempAscii);
+      } else if (el.charCodeAt(0) > 96 && el.charCodeAt(0) < 123) {
+        tempAscii =
+          el.charCodeAt(0) + n > 122
+            ? Math.abs(el.charCodeAt(0) + n - 122) + 96
+            : el.charCodeAt(0) + n;
+        return String.fromCharCode(tempAscii);
+      }
+    })
+    .join("");
+}
+
+//이상한 문자열 만들기
+function strangerString(s) {
+  return s
+    .split("")
+    .map((str) => {
+      return str.split("").map((c, idx) => {
+        return idx % 2 === 0 ? c.toUpperCase() : c.toLowerCase();
+      });
+    })
+    .join(" ");
+}
+
+//키패드 누르기
+function keypadHand(numbers, hand) {
+  const leftSide = [1, 4, 7];
+  const rightSide = [3, 6, 9];
+
+  const leftDiff = {
+    1: { 2: 1, 5: 1.5, 8: 2.5, 0: 3.5 },
+    4: { 2: 1.5, 5: 1, 8: 1.5, 0: 2.5 },
+    7: { 2: 2.5, 5: 1.5, 8: 1, 0: 1.5 },
+    2: { 2: 0, 5: 1, 8: 2, 0: 3 },
+    5: { 2: 1, 5: 0, 8: 1, 0: 2 },
+    8: { 2: 2, 5: 1, 8: 0, 0: 1 },
+    0: { 2: 3, 5: 2, 8: 1, 0: 0 },
+    "*": { 2: 4, 5: 2, 8: 2, 0: 1 },
+  };
+  const rightDiff = {
+    3: { 2: 1, 5: 1.5, 8: 2.5, 0: 3.5 },
+    6: { 2: 1.5, 5: 1, 8: 1.5, 0: 2.5 },
+    9: { 2: 2.5, 5: 1.5, 8: 1, 0: 1.5 },
+    2: { 2: 0, 5: 1, 8: 2, 0: 3 },
+    5: { 2: 1, 5: 0, 8: 1, 0: 2 },
+    8: { 2: 2, 5: 1, 8: 0, 0: 1 },
+    0: { 2: 3, 5: 2, 8: 1, 0: 0 },
+    "#": { 2: 4, 5: 2, 8: 2, 0: 1 },
+  };
+  let left = "*";
+  let right = "#";
+  let answer = "";
+
+  numbers.forEach((num) => {
+    if (leftSide.includes(num)) {
+      left = num;
+      answer += "L";
+    } else if (rightSide.includes(num)) {
+      right = num;
+      answer += "R";
+    } else {
+      const leftDistance = leftDiff[left][num];
+      const rightDistance = rightDiff[right][num];
+      console.log(leftDistance, rightDistance);
+      if (leftDistance < rightDistance) {
+        left = num;
+        answer += "L";
+      } else if (rightDistance < leftDistance) {
+        right = num;
+        answer += "R";
+      } else {
+        hand === "left"
+          ? (() => {
+              left = num;
+              answer += "L";
+            })()
+          : (() => {
+              right = num;
+              answer += "R";
+            })();
+      }
+    }
+    console.log(num, left, right, answer);
+  });
+
+  return answer;
+}
+
+keypadHand([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right");
