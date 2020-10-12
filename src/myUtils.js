f71a210c194ca05deb1cd99538c6785a61f35ed9
aredra <rnswn48@gmail.com>
2020-10-12 20:29:32 +0900

    Add date util

--- src/myUtils.js
+++ src/myUtils.js
@@ -1,3 +1,4 @@
+// 날짜 기간 차이 구하기
 const formatDuration = (ms) => {
   if (ms < 0) ms = -ms;
   const time = {
@@ -11,4 +12,17 @@ const formatDuration = (ms) => {
     .filter((val) => val[1] !== 0)
     .map(([key, val]) => `${val} ${key}${val !== 1 ? "s" : ""}`)
     .join(", ");
+};
+
+// 해당월 마지막 날짜 구하기
+const lastDateOfMonth = (date = new Date()) => {
+  let d = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  return d.toISOString().split("T")[0];
+};
+
+// 현재로부터 몇일 후 날짜 구하기
+const daysFromNow = (n) => {
+  let d = new Date();
+  d.setDate(d.getDate() + Math.abs(n));
+  return d.toISOString().split("T")[0];
 };
