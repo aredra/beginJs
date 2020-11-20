2b2c3dc44cd60eb279978c05739e72f33b3eed85
aredra <rnswn48@gmail.com>
2020-11-20 14:36:31 +0900

    Add about scroll event

--- src/myUtils.js
+++ src/myUtils.js
@@ -42,3 +42,100 @@ const repeatGenerator = function* (val) {
     if (newV !== undefined) v = newV;
   }
 };
+
+// 쓰로틀링 매주기마다 주기적 요청
+const throttle = (fn, wait) => {
+  let inThrottle, lastFn, lastTime;
+  return function () {
+    const context = this,
+      args = arguments;
+    if (!inThrottle) {
+      fn.apply(context, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(function () {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
+};
+
+// 디바운싱 일정 시간 동안 입력이 더이상 발생하지 않을 때 요청
+const debounce = (fn, ms = 0) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
+
+// rgbObject
+const toRGBObject = (rgbStr) => {
+  const [red, green, blue] = rgbStr.match(/\d+/g).map(Number);
+  return { red, green, blue };
+};
+
+// camelCase
+const toCamelCase = (str) => {
+  let s =
+    str &&
+    str
+      .match(
+        /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+      )
+      .map((x) => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
+      .join("");
+  return s.slice(0, 1).toLowerCase() + s.slice(1);
+};
+
+// orderBy
+const orderBy = (arr, props, orders) =>
+  [...arr].sort((a, b) =>
+    props.reduce((acc, prop, i) => {
+      if (acc === 0) {
+        const [p1, p2] =
+          orders && orders[i] === "desc"
+            ? [b[prop], a[prop]]
+            : [a[prop], b[prop]];
+        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
+      }
+      return acc;
+    }, 0)
+  );
+
+// 스크롤이벤트 관련
+var lastScrollTop = 0;
+var delta = 5;
+var fixBox = document.querySelector("#id");
+var fixBoxHeight = fixBox.offsetHeight;
+var didScroll;
+
+window.onscroll = function (e) {
+  didScroll = true;
+};
+
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+function hasScrolled(){
+    var nowScrollTop = window.scrollY;
+    if(Math.abs(lastScrollTop - nowScrollTop) <= delta){
+        return;
+    }
+    if(nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight){
+        //Scroll down
+    }else{
+        if(nowScrollTop + window.innerHeight < document.body.offsetHeight){
+        	//Scroll up
+        }
+    }
+    lastScrollTop = nowScrollTop;
+}
--- src/prgrms1/level1.js
+++ src/prgrms1/level1.js
@@ -301,3 +301,25 @@ const favoriteSubjects = (subjects) => {
 
   return text;
 };
+
+function solution(dartResult) {
+  const bonus = { S: 1, D: 2, T: 3 };
+  let darts = dartResult.match(/\d.?\D/g);
+
+  for (let i = 0; i < darts.length; i++) {
+    let split = darts[i].match(/(\d{1,})([SDT])([*#])?/);
+    let score = Math.pow(split[1], bonus[split[2]]);
+
+    if (split[3] !== undefined) {
+      if (split[3] === "*") {
+        score *= 2;
+
+        if (i > 0) darts[i - 1] *= 2;
+      } else score *= -1;
+    }
+
+    darts[i] = score;
+  }
+
+  return darts.reduce((a, b) => a + b);
+}
